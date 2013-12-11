d55759de5fb622b0f3039e39f5f5e0e642a5e33d
Mike Garabedian <mike.garabedian@riverbed.com>
2013-12-11 08:04:26 -0500

    Fix windows installation script

--- provisioning/windows_provision.sh
+++ provisioning/windows_provision.sh
@@ -2,6 +2,8 @@
 
 # Run ansible commands from within the guest
 
+sudo apt-get update
+sudo apt-get -y install build-essential python-setuptools python-dev python-pip
 sudo pip install ansible
 sudo mkdir /etc/ansible &> /dev/null
 echo localhost | sudo tee /etc/ansible/hosts &> /dev/null
